575796d0ef720af88f707cc75024218e4900c1b4
Farah Arar <farah02arar@gmail.com>
2023-10-30 17:33:05 +0300

    Done

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import DogList from '../Components/DogList/DogList'
 import Form from '../Components/Form/Form'
 import { Inter } from 'next/font/google'
+import { useState, useEffect } from 'react'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -8,6 +9,19 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   // You will need to put a state here to save all the dogs data into
   // And you will fetch the data with useEffect
+  const[image,setImage]=useState(null)
+
+  useEffect(()=>{
+    fetch('https://dog.ceo/api/breeds/image/random/3')
+    .then(r =>r.json())
+    .then(data => setImage(data.image))
+  })
+  const handleFetchImages = () => {
+    // This function can be used to fetch new images, for example, when a button is clicked.
+    fetch('https://dog.ceo/api/breeds/image/random/3')
+      .then((response) => response.json())
+      .then((data) => setImage(data.message))
+  };
 
    return (
     <div className="card">
@@ -17,7 +31,8 @@ export default function Home() {
       */}
       {/* <Form /> Uncomment <Form /> if you are going after the bonus */}
       {/* This page should receive the images array */}
-      <DogList />
+
+      <DogList image={image} />
     </div>
   );
 }
